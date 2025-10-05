e481cb37211b96c94da35f1662f4941c6e9ec29e
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-10-05 18:00:59 +0200

    collection-string

--- Main/Collections.cs
+++ Main/Collections.cs
@@ -25,29 +25,38 @@ namespace oppgaven.Main
                 {
                     Console.WriteLine(item);
                 }
+                Console.WriteLine("Enter your item: ");
+                userInput = Console.ReadLine() ?? string.Empty;
+
+                if (shoppingList.Contains(userInput.ToLower()))
+                    shoppingList.Remove(userInput.ToLower());
+                else
+                    shoppingList.Add(userInput.ToLower());
             }
-        }
-        public void ListInt()
-        {
-            List<int> shoppingInt = new List<int>
-            {
-                2,
-                3,
-                4,
-                5
-            };
-            int userInput;
-        }
-        public void ListDouble()
-        {
-            List<double> shoppingDouble = new List<double>
-            {
-                2.223,
-                3.323,
-                4.232,
-                5.232
-            };
-            double userInput;
         }
     }
 }
+//         public void ListInt()
+//         {
+//             List<int> shoppingInt = new List<int>
+//             {
+//                 2,
+//                 3,
+//                 4,
+//                 5
+//             };
+//             int userInput;
+//         }
+//         public void ListDouble()
+//         {
+//             List<double> shoppingDouble = new List<double>
+//             {
+//                 2.223,
+//                 3.323,
+//                 4.232,
+//                 5.232
+//             };
+//             double userInput;
+//         }
+//     }
+// }
